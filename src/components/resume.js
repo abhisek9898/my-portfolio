93fe2a12a38324daf523b23ac9558ec7dcab816e
Abhisek Malla <abhisek.malla16@gmail.com>
2024-03-07 12:43:15 +0530

    resume modified

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,26 +1,30 @@
-import React from 'react';
-
-
+import React from "react";
 
 const ResumeDownloadButton = () => {
   const downloadResume = () => {
-    const link = document.createElement('a');
-    link.href = '/my-resume.pdf'; 
-    link.download = 'my-resume.pdf';
+    const link = document.createElement("a");
+    link.href = "/My Resume.pdf";
+    link.download = "My Resume.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
   return (
-    <button onClick={downloadResume} style={{
-      padding:'1%',
-      color:'#000',
-      fontSize:20,backgroundColor:'lightblue',
-      border: '2px solid #777',fontWeight:'bold',fontFamily:'Georgia, serif',cursor:'pointer',
+    <button
+      onClick={downloadResume}
+      style={{
+        padding: "1%",
+        color: "#000",
+        fontSize: 20,
+        backgroundColor: "lightblue",
+        border: "2px solid #777",
+        fontWeight: "bold",
+        fontFamily: "Georgia, serif",
+        cursor: "pointer",
       }}
-      >
-     My Resume
+    >
+      My Resume
     </button>
   );
 };
